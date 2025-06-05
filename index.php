1d60b50567a74dcef022062bf1904d6e71513baa
Andres <TheNezdark@gmail.com>
2025-06-05 15:28:13 -0500

    asd

--- index.php
+++ index.php
@@ -8,9 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-<?php
-include 'views/header.php';
-?>
+<?php include 'views/header.php'; ?>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
