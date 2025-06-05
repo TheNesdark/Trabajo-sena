30216a0bcb6119fe7c7f6a6e4dc83c52cb8abdc0
Andres <TheNezdark@gmail.com>
2025-06-05 15:27:27 -0500

    sdfsdf

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,23 +8,10 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-  <div class="container-fluid">
-    <a class="navbar-brand active" href="index.php">Almacén</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav me-auto">
-        <li class="nav-item"><a class="nav-link" href="productos/productos_listar.php">Productos</a></li>
-        <li class="nav-item"><a class="nav-link" href="Fabricante/fabricante_listar.php">Fabricantes</a></li>
-        <li class="nav-item"><a class="nav-link" href="Usuarios/usuario_listar.php">Usuarios</a></li>
-      </ul>
-      <span class="navbar-text me-3"><?= htmlspecialchars($_SESSION['usuario']) ?></span>
-      <a href="logout.php" class="btn btn-outline-light btn-sm">Cerrar sesión</a>
-    </div>
-  </div>
-</nav>
+<?php
+include 'views/header.php';
+?>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
