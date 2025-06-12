9cefd05de95926c4d0044e0f6efb4569f340c7af
Andres <TheNezdark@gmail.com>
2025-06-12 14:39:21 -0500

    fix: update search input placeholder in Busquedas.php for clarity

--- views/Busquedas.php
+++ views/Busquedas.php
@@ -1,6 +1,6 @@
 <div class="col-md-9 col-12 mb-2 mb-md-0">
     <form class="d-flex w-100" method="GET">
-        <input class="form-control me-2" type="search" name="busqueda" placeholder="Buscar Aprendiz" aria-label="Search">
+        <input class="form-control me-2" type="search" name="busqueda" placeholder="Buscar" aria-label="Search">
         <button class="btn btn-outline-success" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
     </form>
 </div>
