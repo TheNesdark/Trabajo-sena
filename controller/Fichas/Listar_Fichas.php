07d12704f1aa435af350f736fb2b308824acc24a
Andres <TheNezdark@gmail.com>
2025-06-12 14:44:30 -0500

    fix: improve search query preparation in listarFichas function for better performance

--- controller/Fichas/Listar_Fichas.php
+++ controller/Fichas/Listar_Fichas.php
@@ -5,9 +5,9 @@ require '../config.php';
 function listarFichas() {
     global $pdo;
     $busqueda = '';
-    if (isset($_GET['busqueda'])) {
+    if (isset($_GET['busqueda']) && $_GET['busqueda'] !== "") {
         $busqueda = $_GET['busqueda'];
-        $stmt = $pdo->query("SELECT programa.nombreprograma, ficha.nficha FROM ficha INNER JOIN programa ON ficha.idprograma = programa.idprograma WHERE programa.nombreprograma LIKE :busqueda OR ficha.nficha LIKE :busqueda");
+        $stmt = $pdo->prepare("SELECT programa.nombreprograma, ficha.nficha FROM ficha INNER JOIN programa ON ficha.idprograma = programa.idprograma WHERE programa.nombreprograma LIKE :busqueda OR ficha.nficha LIKE :busqueda");
         $stmt->execute([':busqueda' => "%$busqueda%"]);
     } else {
         $stmt = $pdo->query("SELECT programa.nombreprograma, ficha.nficha FROM ficha INNER JOIN programa ON ficha.idprograma = programa.idprograma");
