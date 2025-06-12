a2341e6ca3592f378c8fb722662d5fcf69687495
uselobien <uselobien85@gmail.com>
2025-06-12 13:52:08 -0500

    fix: mejorar la funcionalidad de búsqueda de aprendices y actualizar la estructura del formulario

--- controller/Aprendices/Listar_Aprendices.php
+++ controller/Aprendices/Listar_Aprendices.php
@@ -5,7 +5,7 @@ function listarAprendices() {
     $busqueda = '';
     if (isset($_GET['busqueda'])) {
         $busqueda = $_GET['busqueda'];
-        $stmt = $pdo->prepare("SELECT * FROM aprendiz WHERE nombres LIKE :busqueda OR apellidos LIKE :busqueda");
+        $stmt = $pdo->prepare("SELECT * FROM aprendiz WHERE nombres LIKE :busqueda OR apellidos LIKE :busqueda or idaprendiz LIKE :busqueda");
         $stmt->execute([':busqueda' => "%$busqueda%"]);
     } else {
         $stmt = $pdo->query("SELECT * FROM aprendiz");
@@ -15,11 +15,9 @@ function listarAprendices() {
 }
 ?>
 
-<form action="" method="GET" class="row g-2 mb-4">
-    <div class="col-auto">
-        <input type="text" class="form-control" name="busqueda" placeholder="Buscar aprendices" value="<?php echo htmlspecialchars($busqueda); ?>">
-    </div>
-    <div class="col-auto">
-        <button type="submit" class="btn btn-primary">Buscar</button>
-    </div>
-</form>
+<div class="col-md-9 col-12 mb-2 mb-md-0">
+            <form class="d-flex w-100" method="GET">
+                <input class="form-control me-2" type="search" name="busqueda" placeholder="Buscar Aprendiz" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+            </form>
+        </div>
--- views/aprendiz.php
+++ views/aprendiz.php
@@ -11,18 +11,21 @@
 <body>
 <?php
 include 'header.php';
-include '../controller/Aprendices/Listar_Aprendices.php';
 include '../controller/Aprendices/Modals.php';
 ?>
 <div id="alerta"></div>
 <div class="container mt-4">
+    
     <div class="mb-2 mt-4 mrb-4 d-flex justify-content-center">
         <h1> <i class="fa-solid fa-graduation-cap" style="color: #50c8c6"></i> Lista de Aprendices</h1>
     </div>
-    <div class="d-flex justify-content-end mb-2" style="margin-right: 20px;">
-        <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#addAprendizModal">
-            <i class="fa-solid fa-plus"></i> Añadir Aprendiz
-        </button>
+    <div class="row mb-2" style="max-width: 98%; margin:auto;">
+        <?php include '../controller/Aprendices/Listar_Aprendices.php'; ?>
+        <div class="col-md-3 col-12 d-flex justify-content-md-end justify-content-center">
+            <button type="button" class="btn btn-success w-100" data-bs-toggle="modal" data-bs-target="#addAprendizModal">
+                <i class="fa-solid fa-plus"></i> Añadir Aprendiz
+            </button>
+        </div>
     </div>
     <div class="table-container" style="max-width: 98%; margin:auto;">
         <div class="table-responsive">
