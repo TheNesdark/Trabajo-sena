ddefc2e31b2d6c56bae12a4b1ca0ffbba975ded7
uselobien <uselobien85@gmail.com>
2025-06-13 14:36:23 -0500

    fix: corregir la inserción de acciones para usar el usuario de la sesión

--- controller/Acciones/Añadir_Acciones.php
+++ controller/Acciones/Añadir_Acciones.php
@@ -1,13 +1,16 @@
 <?php
-
+session_start();
 include '../../config.php';
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $descripcion = $_POST['descripcion'];
     $idreporte = $_POST['idreporte'];
+    $usuario = $_SESSION['usuario'];
+
     try {
-        $stmt = $pdo->prepare("INSERT INTO acciones (idreporte,fecha,descripcion,usuario) VALUES ( ?, now(), ?, 'andresB')");
-        $stmt->execute([$idreporte, $descripcion]);
-        header("Location: ../../views/acciones.php?mensaje=agregado&idreporte=$idreporte&aprendiz=$idaprendiz");
+        $stmt = $pdo->prepare("INSERT INTO acciones (idreporte, fecha, descripcion, usuario) VALUES (?, now(), ?, ?)");
+        $stmt->execute([$idreporte, $descripcion, $usuario]);
+        header("Location: ../../views/acciones.php?mensaje=agregado&idreporte=$idreporte");
         exit();
     } catch (PDOException $e) {
         die("Error al agregar la acción: " . $e->getMessage());
